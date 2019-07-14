19dee9e202baed7b3aa87fa6a5286f4cae5ed3e4
StackSmack007 <n_hristov007@abv.bg>
2019-07-14 22:39:15 +0300

    Created interfaces and classes for ActionResults

---
+++ WebLab1.0/SIS/SIS/SIS.Framework/ActionResults/ViewResult.cs
@@ -0,0 +1,18 @@
+﻿namespace SIS.Framework.ActionResults
+{
+    using SIS.Framework.ActionResults.Contracts;
+    public class ViewResult : IViewable
+    {
+        public ViewResult(IRenderable view)
+        {
+            this.View = view;
+        }
+
+        public IRenderable View { get; set; }
+
+        public string Invoke()
+        {
+            return View.Render();
+        }
+    }
+}
