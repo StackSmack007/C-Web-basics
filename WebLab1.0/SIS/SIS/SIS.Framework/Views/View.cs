fc1ff3786ee84227a20e006f6d1b1de34b7532a7
StackSmack007 <n_hristov007@abv.bg>
2019-07-14 22:41:58 +0300

    Introduced View Class that is Renderable.

---
+++ WebLab1.0/SIS/SIS/SIS.Framework/Views/View.cs
@@ -0,0 +1,29 @@
+﻿namespace SIS.Framework.Views
+{
+    using SIS.Framework.ActionResults.Contracts;
+    using System;
+    using System.IO;
+    public class View : IRenderable
+    {
+        private readonly string fullyQualifiedTemplateName;
+
+        public View(string fullyQualifiedTemplateName)
+        {
+            this.fullyQualifiedTemplateName = fullyQualifiedTemplateName;
+        }
+
+        private string ReadFile()
+        {
+            if (File.Exists(this.fullyQualifiedTemplateName))
+            {
+                return File.ReadAllText(fullyQualifiedTemplateName);
+            }
+            throw new FileNotFoundException($"File at {fullyQualifiedTemplateName} not found!");
+        }
+
+        public string Render()
+        {
+            return this.ReadFile();
+        }
+    }
+}
