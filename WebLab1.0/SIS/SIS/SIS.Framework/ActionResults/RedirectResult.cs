6b24c1c46db711af32188c25af506a24c15abdb5
StackSmack007 <n_hristov007@abv.bg>
2019-07-14 23:06:25 +0300

    Introduced abstract Controller class with request and View method returning IViewable

---
+++ WebLab1.0/SIS/SIS/SIS.Framework/ActionResults/RedirectResult.cs
@@ -0,0 +1,18 @@
+﻿namespace SIS.Framework.ActionResults
+{
+using SIS.Framework.ActionResults.Contracts;
+    public class RedirectResult : IRedirectable
+    {
+        public string RedirectUrl { get; set; }
+
+        public RedirectResult(string url)
+        {
+            RedirectUrl = url;
+        }
+
+        public string Invoke()
+        {
+            return this.RedirectUrl;
+        }
+    }
+}
---
+++ WebLab1.0/SIS/SIS/SIS.Framework/Controllers/Controller.cs
@@ -0,0 +1,27 @@
+﻿namespace SIS.Framework.Controllers
+{
+    using SIS.Framework.ActionResults;
+    using SIS.Framework.ActionResults.Contracts;
+    using SIS.Framework.Utilities;
+    using SIS.Framework.Views;
+    using SIS.HTTP.Requests.Contracts;
+    using System.Runtime.CompilerServices;
+
+    public abstract class Controller
+    {
+        protected Controller() { }
+
+        public IHttpRequest Request { get; set; }
+
+        protected IViewable View([CallerMemberName] string viewName = "")
+        {
+            string controllerNameClean = ControllerUtilities.GetControllerName(this);
+
+            string fullyQualifiedName = ControllerUtilities.GetViewFullQualifiedName(controllerNameClean, viewName);
+
+            IRenderable view = new View(fullyQualifiedName);
+
+            return new ViewResult(view);
+        }
+    }
+}
