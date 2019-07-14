3fd992b67c881a8043c218b9793141402f07ce39
StackSmack007 <n_hristov007@abv.bg>
2019-07-14 22:44:10 +0300

    Introduced MvcContext that self instances upon request and is alterable. Stores global string constants such as paths and names of things.

---
+++ WebLab1.0/SIS/SIS/SIS.Framework/MvcContext.cs
@@ -0,0 +1,32 @@
+﻿namespace SIS.Framework
+{
+    public class MvcContext
+    {
+        private static MvcContext instance;
+
+        private MvcContext() { }
+
+        public static MvcContext Get
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    instance = new MvcContext();
+                }
+                return instance;
+            }
+        }
+
+public string AssemblyName { get; set; }
+
+        public string ControllersFolder { get; set; } = "../../../Controllers";//TODOCHECK
+
+        public string ControllerSuffix { get; set; } = "Controller";
+
+        public string ViewsFolder { get; set; }= "Views";//TODOCHECK
+
+        public string ModelsFolder { get; set; } = "Models";
+
+    }
+}
