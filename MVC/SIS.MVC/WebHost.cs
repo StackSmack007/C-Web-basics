eea0e00dde8e1ddf7fa4d080a5f13b3400f46c6f
StackSmack007 <n_hristov007@abv.bg>
2019-07-26 21:29:57 +0300

    Introduced Auto-Routing by Conventional names of controllers and methods.

--- MVC/SIS.MVC/WebHost.cs
+++ MVC/SIS.MVC/WebHost.cs
@@ -33,33 +33,52 @@
             serverRoutingTable = new ServerRoutingTable();
             serviceContainer = new ServiceCollection();
             logger = new ConsoleLogger();
-          //  ClearAllTemps();
         }
         public static IServiceCollection ServiceContainer => serviceContainer;
-
-     //  private static void ClearAllTemps()
-     //  {
-     //      DirectoryInfo di = new DirectoryInfo(@"C:\Users\ZEVS\source\Web Basics\MVC Lections\MVC\SIS.MVC\ViewEngine\GeneratedModels\");
-     //
-     //      foreach (FileInfo file in di.GetFiles())
-     //      {
-     //          file.Delete();
-     //      }
-     //      foreach (DirectoryInfo dir in di.GetDirectories())
-     //      {
-     //          dir.Delete(true);
-     //      }
-     //  }
 
         public static void Start(int port, IMvcApplication application)
         {
 
             ConfigureRoutingFromAttributes();
             application.ConfigureRouting(serverRoutingTable);
+            ConfigureRoutesByNamesConvention();
             application.ConfigureServices(serviceContainer);
             serviceContainer.AddService<IViewEngine, View_Engine>();
             Server server = new Server(port, serverRoutingTable);
             server.Run();
+        }
+
+        /// <summary>
+        /// Makes Routings via controller and method Names if possible if no other route is given!
+        /// </summary>
+        private static void ConfigureRoutesByNamesConvention()
+        {
+            Type[] controllerClassesWithUnregistredMethods = Assembly.GetEntryAssembly().GetTypes().Where(x => x.IsSubclassOf(typeof(Controller)) && !x.IsAbstract)
+                .Where(x => x.GetMethods().Any(m => m.ReturnType == typeof(IHttpResponse) &&
+                      (!m.GetCustomAttributes<HttpAttribute>().Any() || m.GetCustomAttributes<HttpAttribute>().Any(at => at.Path == null)))).ToArray();
+
+            if (!controllerClassesWithUnregistredMethods.Any()) return;
+
+            foreach (Type controllerType in controllerClassesWithUnregistredMethods)
+            {
+                string viewFolderName = controllerType.Name.Replace("Controller", "");
+                foreach (MethodInfo methodInfo in controllerType.GetMethods().Where(m => m.ReturnType == typeof(IHttpResponse) && !m.GetCustomAttributes<HttpAttribute>().Any()))
+                {//case of no HttpAttribute
+                    HttpRequestMethod defaultMethod = HttpRequestMethod.Get;
+                    string path = "/" + viewFolderName + "/" + methodInfo.Name;
+                    if (serverRoutingTable.Routes.SelectMany(x => x.Value.Keys).Any(x => x == path)) continue; //the route is registered manually so it is skipped!
+
+                    EnlistRoute(defaultMethod, path, controllerType, methodInfo);
+                }
+                foreach (MethodInfo methodInfo in controllerType.GetMethods().Where(m => m.ReturnType == typeof(IHttpResponse) && m.GetCustomAttributes<HttpAttribute>().Any(x=>x.Path == null)))
+                {//case of pathless HttpAttribute (HttpPostMost likely)
+                    string path = "/" + viewFolderName + "/" + methodInfo.Name;
+                    foreach (var methodType in methodInfo.GetCustomAttributes<HttpAttribute>().Where(x => x.Path == null).Select(x=>x.MethodType))
+                    {
+                        EnlistRoute(methodType, path, controllerType, methodInfo);
+                    }
+                }
+            }
         }
 
         private static void ConfigureRoutingFromAttributes()
@@ -69,13 +88,13 @@
             foreach (Type controller in controllerClasses)
             {
                 MethodInfo[] actionMethods = controller.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                                                       .Where(x => x.GetCustomAttributes<HttpAttribute>().Any()).ToArray();
+                                                       .Where(x => x.GetCustomAttributes<HttpAttribute>().Where(at=>at.Path!=null).Any()).ToArray();
 
                 foreach (MethodInfo methodInfo in actionMethods)
                 {
                     HttpAttribute[] attributes = methodInfo.GetCustomAttributes<HttpAttribute>().ToArray();
 
-                    foreach (HttpAttribute attribute in attributes)
+                    foreach (HttpAttribute attribute in attributes.Where(at => at.Path != null))
                     {
                         HttpRequestMethod methodType = attribute.MethodType;
                         string path = attribute.Path;
@@ -116,7 +135,7 @@
                     FillData(parametersData, actionParameters, providedParameters);
                 }
                 #endregion
-                
+
                 return (IHttpResponse)methodInfo.Invoke(controllerInstance, parametersData.ToArray());
             };
         }
@@ -132,7 +151,7 @@
                     if (!providedParameters.ContainsKey(requiredParam.Name))
                     {
                         logger.Log($"parameter {requiredParam.Name} was not provided. The value in method is set to null!");
-                       parametersData.Add(null);
+                        parametersData.Add(null);
                         continue;
                     }
                     else if (providedParameters[requiredParam.Name].GetType() != requiredParam.ParameterType)
@@ -153,12 +172,12 @@
                     parametersData.Add(providedParameters[requiredParam.Name]);
                     continue;
                 }
-       
+
                 ConstructorInfo subClassConstructor = requiredParam.ParameterType.GetConstructors()
                                                          .Where(x => x.GetParameters().All(p => providedParameters.ContainsKey(p.Name)))
                                                          .OrderByDescending(x => x.GetParameters().Count())
                                                          .FirstOrDefault();
-      
+
                 if (subClassConstructor is null)
                 {
                     logger.Log($"Parameter {requiredParam.Name} was a class and had not all parameters provided.The value in method is set to null!");
