c61b4a9e00bf69c8d13726e1c045c58741882d9d
StackSmack007 <n_hristov007@abv.bg>
2019-07-28 12:38:40 +0300

    Added description of method {void AddService<T1, T2>(params object[] parameters)}

--- MVC/SIS.MVC/Contracts/IServiceCollection.cs
+++ MVC/SIS.MVC/Contracts/IServiceCollection.cs
@@ -3,6 +3,11 @@
 using System;
     public interface IServiceCollection
     {
+        /// <summary>
+        /// </summary>
+        /// <typeparam name="T1">InterfaceName</typeparam>
+        /// <typeparam name="T2">ClassName</typeparam>
+        /// <param name="parameters">Parameters if ctor needs them!</param>
         void AddService<T1, T2>(params object[] parameters);
 
         void AddService<T1>(Func<object> action);
