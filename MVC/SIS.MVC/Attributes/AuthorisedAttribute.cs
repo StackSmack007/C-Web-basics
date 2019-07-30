ced5e92c96b1d42571c2d7efed81c783cb5e0f7d
StackSmack007 <n_hristov007@abv.bg>
2019-07-31 00:54:31 +0300

    Added Authorised attribute it can accept path or use default path set in controller class

---
+++ MVC/SIS.MVC/Attributes/AuthorisedAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace SIS.MVC.Attributes
+{
+    using System;
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class AuthorisedAttribute : Attribute
+    {
+        public string AltPath { get; }
+        public AuthorisedAttribute()
+        {
+            AltPath = null;
+        }
+
+        public AuthorisedAttribute(string alternativePath)
+        {
+            AltPath = alternativePath;
+        }
+    }
+}
