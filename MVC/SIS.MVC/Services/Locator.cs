eec2cc884f035d029d8e2e95d7673558fc95fa3a
StackSmack007 <n_hristov007@abv.bg>
2019-07-30 18:28:32 +0300

    Added Locator class that locates subFolderPath of subFilePath by going going backward.

---
+++ MVC/SIS.MVC/Services/Locator.cs
@@ -0,0 +1,51 @@
+﻿namespace SIS.MVC.Services
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Locator
+    {
+        static Locator()
+        {
+            CurrentLocation = Assembly.GetAssembly(typeof(Locator)).Location;
+        }
+
+        public static string CurrentLocation { get; set; }
+
+        public static string GetPathOfFile(string destination, string filename)
+        {
+            return GetPathOfFolder(destination) + "\\" + filename;
+        }
+
+        public static string GetPathOfFolder(string destination)
+        {
+            destination = destination.TrimEnd('\\', '/');
+            string[] foldersToBeContained = destination.Split(new char[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            string currentPath = Path.GetFullPath(Path.Combine(CurrentLocation, $"../"));
+
+            while (!FoldersAreContained(currentPath, foldersToBeContained))
+            {
+                currentPath = Path.GetFullPath(Path.Combine(currentPath, $"../"));
+            }
+            currentPath = Path.GetFullPath(Path.Combine(currentPath, destination));
+            return currentPath;
+        }
+
+        private static bool FoldersAreContained(string path, string[] foldersToBeContained)
+        {
+            bool result = foldersToBeContained.Any();
+            foreach (string folder in foldersToBeContained)
+            {
+                path = Path.GetFullPath(Path.Combine(path, $"./{folder}"));
+                if (!Directory.Exists(path))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
