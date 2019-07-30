b7caedc6af72c4ae947d42b11ab94ab9d0395930
StackSmack007 <n_hristov007@abv.bg>
2019-07-30 18:35:30 +0300

    Extebded Handling Response to externall class in WebServer library

---
+++ MVC/SIS.WebServer/Api/HttpHandler.cs
@@ -0,0 +1,43 @@
+﻿using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses;
+using SIS.HTTP.Responses.Contracts;
+using SIS.WebServer.Api.Contracts;
+using SIS.WebServer.Results;
+using SIS.WebServer.Routing;
+namespace SIS.WebServer.Api
+{
+    using System.IO;
+    using System.Net;
+    public class HttpHandler : IHttpHandler
+    {
+        private ServerRoutingTable serverRoutingTable;
+
+        public HttpHandler(ServerRoutingTable serverRoutingTable)
+        {
+            this.serverRoutingTable = serverRoutingTable;
+        }
+
+        public IHttpResponse Handle(IHttpRequest httpRequest)
+        {
+              if (!this.serverRoutingTable.Routes.ContainsKey(httpRequest.RequestMethod) ||
+                    !this.serverRoutingTable.Routes[httpRequest.RequestMethod].ContainsKey(httpRequest.Path))
+                {
+                    return ReturnIfResource(httpRequest.Path);
+
+                }
+                return this.serverRoutingTable.Routes[httpRequest.RequestMethod][httpRequest.Path].Invoke(httpRequest);
+        }
+
+        private IHttpResponse ReturnIfResource(string path)
+        {   
+            path = "../../../.." + path;
+            if (File.Exists(path))
+            {
+                byte[] content = File.ReadAllBytes(path);
+                var response = new InlineResourseResult(content, HttpStatusCode.OK);
+                return response;
+            }
+            return new HttpResponse(HttpStatusCode.NotFound);
+        }
+    }
+}
