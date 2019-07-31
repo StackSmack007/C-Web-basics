97e9450cabf7d04273ee0588953958459b891b87
StackSmack007 <n_hristov007@abv.bg>
2019-07-31 19:38:32 +0300

    unsignificant changes

---
+++ MVC/SIS.MVC/Services/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace SIS.MVC.Services
+{
+    using SIS.MVC.Contracts;
+    public class Configuration : IConfiguration
+    {
+        public string DefaultAuthorisedRedirectAdress { get; set; } = "/Users/Login";
+        public string LayoutsFolderPath { get; set; } = @"/Views/Layouts/";
+        public string KeywordForInsertingBodyInImportLayout { get; set; } = "@BodyContent@";
+        public string LocationOfViewsFolder { get; set; } = @"/Views/";
+        public string LocationOfRootFolder { get; set; } = @"/Root/";
+    }
+}
