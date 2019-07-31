0e4eeba09858c6205acd8bb5c39a920931555896
StackSmack007 <n_hristov007@abv.bg>
2019-07-31 19:35:57 +0300

    Added class for static configuration from outside, All path constants are in it.

---
+++ MVC/SIS.MVC/Contracts/IConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace SIS.MVC.Contracts
+{
+    public interface IConfiguration
+    {
+        string DefaultAuthorisedRedirectAdress { get; set; }
+        string LayoutsFolderPath { get; set; }
+        string KeywordForInsertingBodyInImportLayout { get; set; }
+        string LocationOfViewsFolder { get; set; }
+        string LocationOfRootFolder { get; set; }
+    }
+}
